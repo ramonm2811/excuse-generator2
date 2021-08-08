454434cab7241a48ad1aad6fecaec223d4807370
Ramon Muñoz <62648414+ramonm2811@users.noreply.github.com>
2021-08-08 18:36:22 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let articulo = ["Un", "El"];
+  let sujeto = ["pollo", "señor", "perro", "loco", "niño"];
+  let accion = ["tomo", "me lanzó", "dijo que", "me robó", "me mordió"];
+  let que = ["la tarea", "el telefono", "el carro", "el zapato"];
+  let donde = ["en la calle", "en mi casa", "en el metro"];
+
+  let proIndx = Math.floor(Math.random() * articulo.length);
+  let subjIndx = Math.floor(Math.random() * sujeto.length);
+  let actIndx = Math.floor(Math.random() * accion.length);
+  let possIndx = Math.floor(Math.random() * que.length);
+  let wheIndx = Math.floor(Math.random() * donde.length);
+
+  return (
+    articulo[proIndx] +
+    " " +
+    sujeto[subjIndx] +
+    " " +
+    accion[actIndx] +
+    " " +
+    que[possIndx] +
+    " " +
+    donde[wheIndx]
+  );
 };
